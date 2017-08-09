16801cc4a92adf07835f3056f6992e7434a48c7e
Otari Kobiashvili <otarikobi@gmail.com>
2017-08-09 14:37:35 -0400

    Add time since feature for when campgrounds and comments were added

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.use(express.static(__dirname + "/public"));
 app.use(flash());
-// seedDB();
+app.locals.moment = require('moment');
 
 //PASSPORT config
 app.use(require("express-session")({
