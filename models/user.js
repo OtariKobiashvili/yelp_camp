407040be01fa84ad0c1cc720e0ea8ad2388569d4
Otari Kobiashvili <otarikobi@gmail.com>
2017-08-22 13:11:44 -0400

    Update userSchema to add more data about user

--- models/user.js
+++ models/user.js
@@ -4,6 +4,11 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var userSchema = mongoose.Schema({
   username: String,
   password: String,
+  avatar: {type: String, default: "https://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png"},
+  firstName: {type: String, default: "First Name"},
+  lastName: {type: String, default: "Last Name"},
+  email: {type: String, default: "email@gmail.com"},
+  description: {type: String, default: "Edit your profile to add a description about you."},
   isAdmin: {type: Boolean, default: false}
 });
 
