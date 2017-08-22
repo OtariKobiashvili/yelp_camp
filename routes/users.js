6326332de67b2e95e2f7dfbd991e4f9e47696ebe
Otari Kobiashvili <otarikobi@gmail.com>
2017-08-22 16:07:30 -0400

    Add edit get route Add edit put route Create edit ejs template

--- routes/users.js
+++ routes/users.js
@@ -7,22 +7,50 @@ var campground = require("../models/campground"),
     user = require("../models/user");
 
 router.get("/:id", function(req, res) {
-  console.log(req.params.id)
   user.findById(req.params.id, function(err, foundUser) {
     if(err) {
       req.flash("error", "Something went wrong: " + err);
-      res.redirect("/")
+      res.redirect(".");
     }
     campground.find().where("author.id").equals(foundUser._id).exec(function(err, campgrounds) {
       if(err) {
         req.flash("error", "Something went wrong: " + err);
-        res.redirect("/")
+        res.redirect(".");
       }
-      console.log(campgrounds)
       res.render("users/show", { user : foundUser, campgrounds: campgrounds})
     });
   });
 });
 
+//get edit form
+router.get("/:id/edit", middleware.checkProfileOwnership, function(req, res) {
+  user.findById(req.params.id, function(err, foundUser) {
+    if(err) {
+      req.flash("error", "Something went wrong: " + err);
+      res.redirect(".");
+    }
+    res.render("users/edit", {user: foundUser});
+  });
+});
+
+//put request
+router.put("/:id", middleware.checkProfileOwnership, function(req, res) {
+  var dataToUpdate = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    avatar: req.body.avatar,
+    description: req.body.description
+  }
+  user.findByIdAndUpdate(req.params.id, {$set: dataToUpdate}, function(err, updatedUser) {
+    if(err) {
+      req.flash("error", err.message);
+      res.redirect("/users/" + req.params.id);
+    } else {
+      req.flash("success", "Successfully updated your profile.");
+      res.redirect("/users/" + req.params.id);
+    }
+  })
+})
 
 module.exports = router;
