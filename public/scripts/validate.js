8ca6570aee5158b31bd8342a36242f484714ea68
Otari Kobiashvili <otarikobi@gmail.com>
2017-08-22 16:06:18 -0400

    delete console.logs

--- public/scripts/validate.js
+++ public/scripts/validate.js
@@ -1,11 +1,9 @@
 $("#image-search").on("input", function(){
   var imageSource = $(this).val();
-  console.log(imageSource);
   $(".form-group img").attr("src" , imageSource);
 
   $(".form-group img").on('load', function(e){
     $(".form-group p").html("")
-    console.log('Success!');
   }).on('error', function(e) {
     $(".form-group img + p").html("Error: Please enter a valid image URL.");
     $("#form").on("submit", function(e){
