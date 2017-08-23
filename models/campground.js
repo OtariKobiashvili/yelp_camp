b94a3af6e528550925d31eac9676e5c165312cf6
Otari Kobiashvili <otarikobi@gmail.com>
2017-08-23 16:43:02 -0400

    update models to add ratings

--- models/campground.js
+++ models/campground.js
@@ -22,7 +22,14 @@ var campgroundSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "Comment"
       }
-    ]
+    ],
+    ratings: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Rating"
+        }
+    ],
+    rating: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model("Campground", campgroundSchema);
