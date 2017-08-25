020b9d8594a588383d7f6072d5bb9d6a62d6686c
Otari Kobiashvili <otarikobi@gmail.com>
2017-08-25 14:52:50 -0400

    Allow only images from unspash.com

--- middleware/index.js
+++ middleware/index.js
@@ -65,6 +65,15 @@ middlewareObj.checkProfileOwnership = function(req, res, next){
     }
 }
 
+middlewareObj.isSafe = function(req, res, next) {
+    if(req.body.image.match(/^https:\/\/images\.unsplash\.com\/.*/)) {
+      next();
+    } else {
+      req.flash("error", "Only images from images.unsplash.com allowed.\nSee https://youtu.be/Bn3weNRQRDE for how to copy image urls from unsplash.");
+      res.redirect("back");
+    }
+}
+
 middlewareObj.isLoggedIn = function(req, res, next){
     if(req.isAuthenticated()){
         return next();
