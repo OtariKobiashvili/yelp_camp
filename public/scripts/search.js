3c2eca8bbcd8d1337d4afb3127e1fb54ba29bc1a
Otari Kobiashvili <otarikobi@gmail.com>
2017-08-29 13:42:09 -0400

    Update index page styling/layout

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -3,32 +3,32 @@ $('#campground-search').on('input', function() {
   if(search === "search=") {
     search = "search=all"
   }
-
-  console.log(search);
-  $.get('/campgrounds?' + search, function(data) {
-    $('#campground-grid').html('');
-    $("#search-message").html('');
-      console.log(data);
-      if(data.length < 1){
-        $('#search-message').html(
-          "No campgrounds found. Please try again."
-        )
-      }
-      data.forEach(function(campground) {
-        $('#campground-grid').append(`
-          <div class="col-md-4 col-sm-6">
-            <div class="img-thumbnail">
-              <img class="img-fluid" src="${ campground.image }">
-              <div class="caption">
-                <h5>${ campground.name }</h5>
+  $("#campground-grid").fadeOut(400, function(){
+    $.get('/campgrounds?' + search, function(data) {
+      $('#campground-grid').html('');
+      $("#search-message").css("visibility", "hidden");
+        console.log(data);
+        if(data.length < 1){
+          $('#search-message').css("visibility", "visible");
+        } else {
+          $("#campground-grid").fadeIn(400);
+        }
+        data.forEach(function(campground) {
+          $('#campground-grid').append(`
+            <div class="col-md-4 col-sm-6 campground-card">
+              <div class="img-thumbnail">
+                <img class="img-fluid" src="${ campground.image }">
+                <div class="caption">
+                  <h5>${ campground.name }</h5>
+                </div>
+                <p>
+                  <a href="/campgrounds/${ campground._id }" class="btn btn-sm btn-primary"><i class="fa fa-info-circle" aria-hidden="true"></i> More Info</a>
+                </p>
               </div>
-              <p>
-                <a href="/campgrounds/${ campground._id }" class="btn btn-sm btn-primary"><i class="fa fa-info-circle" aria-hidden="true"></i> More Info</a>
-              </p>
             </div>
-          </div>
-        `);
-      });
+          `);
+        });
+    });
   });
 });
 
